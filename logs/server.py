ed129d8bd78726619c3f645ea7539e95cca8ee01
Ahmed Lekssays <lekssaysahmed@gmail.com>
2022-10-24 23:12:37 +0200

    generate metrics

--- logs/server.py
+++ logs/server.py
@@ -10,7 +10,9 @@ async def hello(websocket, path):
     message = await websocket.recv()
     print(message)
     if "@" in message:
-        write(message=message[1:], filename="metrics.csv")
+        message = message.split("@")
+        message = message[1]
+        write(message=message, filename="metrics.csv")
     write(message=message, filename="system.log")
 
 start_server = websockets.serve(hello, "0.0.0.0", 7777)
