27ff7ea3b0e943b2b62b82987f17bb1573159ef2
royd1990 <royd1990@gmail.com>
2022-11-01 08:29:43 +0000

    Fixed different learning rates for DP and normal.

--- client/src/DPLSTM.py
+++ client/src/DPLSTM.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 from opacus.layers import DPLSTM
 
-class SingleLSTMEncoder(nn.Module):
+class DPLSTMEncoder(nn.Module):
     def __init__(self,n_channels = 52, n_hidden_layers = 128,  n_layers=2, n_classes=11, drop_prob=0.5):
         super().__init__()
         
@@ -12,13 +12,9 @@ class SingleLSTMEncoder(nn.Module):
         self.n_classes = n_classes
         self.drop_prob = drop_prob
         self.n_channels = n_channels
-        if self.n_layers > 1:
-            self.lstm  = nn.LSTM(self.n_channels, self.n_hidden, num_layers=self.n_layers, dropout=self.drop_prob)
-        else:
-            self.lstm  = nn.LSTM(self.n_channels, self.n_hidden, num_layers=self.n_layers, dropout=self.drop_prob)
+        self.lstm  = DPLSTM(self.n_channels, self.n_hidden, num_layers=self.n_layers,bidirectional=False,batch_first=True)
         self.fc = nn.Linear(self.n_hidden, self.n_classes)
         self.dropout = nn.Dropout(self.drop_prob)
-        
         
     def forward(self, x):
         x, _ = self.lstm(x)
--- server/src/SingleLSTM.py
+++ server/src/SingleLSTM.py
@@ -1,7 +1,7 @@
 from torch import nn
 import torch
 import numpy as np
-from opacus.layers import DPLSTM
+from opacus.layers.dp_rnn import DPLSTM
 
 class SingleLSTMEncoder(nn.Module):
     def __init__(self,n_channels = 52, n_hidden_layers = 128,  n_layers=2, n_classes=11, drop_prob=0.5):
@@ -14,9 +14,35 @@ class SingleLSTMEncoder(nn.Module):
         self.n_channels = n_channels
         
         if self.n_layers > 1:
-            self.lstm  = nn.LSTM(self.n_channels, self.n_hidden, self.n_layers, dropout=self.drop_prob)
+            self.lstm  = nn.LSTM(self.n_channels, self.n_hidden, self.n_layers, dropout=self.drop_prob,batch_first=True)
         else:
-            self.lstm  = nn.LSTM(self.n_channels, self.n_hidden, self.n_layers)
+            self.lstm  = nn.LSTM(self.n_channels, self.n_hidden, self.n_layers, dropout=self.drop_prob,batch_first=True)
+        self.fc = nn.Linear(self.n_hidden, self.n_classes)
+        self.dropout = nn.Dropout(self.drop_prob)
+        
+        
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        x = self.dropout(x)    
+        out = self.fc(x)
+        x_op = out[:,-1,:]
+        x_op = torch.softmax(x_op,dim=1)
+        return x_op
+
+class DPLSTMEncoder(nn.Module):
+    def __init__(self,n_channels = 52, n_hidden_layers = 128,  n_layers=2, n_classes=11, drop_prob=0.5):
+        super().__init__()
+        
+        self.n_layers = n_layers
+        self.n_hidden = n_hidden_layers
+        self.n_classes = n_classes
+        self.drop_prob = drop_prob
+        self.n_channels = n_channels
+        
+        if self.n_layers > 1:
+            self.lstm  = DPLSTM(self.n_channels, self.n_hidden, self.n_layers, dropout=self.drop_prob, batch_first=True)
+        else:
+            self.lstm  = DPLSTM(self.n_channels, self.n_hidden, self.n_layers, dropout=self.drop_prob, batch_first=True)
         self.fc = nn.Linear(self.n_hidden, self.n_classes)
         self.dropout = nn.Dropout(self.drop_prob)
         
