a02e65dbd594d3f7aed02562730f8cdb7805031a
royd1990 <royd1990@gmail.com>
2022-12-27 16:54:48 +0100

    FedAvg

--- server/src/main_fed.py
+++ server/src/main_fed.py
@@ -11,8 +11,9 @@ def FedAvg(w):
     w_avg = copy.deepcopy(w[0])
     for k in w_avg.keys():
         for i in range(1, len(w)):
+            w_avg[k] = w_avg[k].detach().cpu()
+            w[i][k]= w[i][k].detach().cpu()
             w_avg[k] += w[i][k]
         w_avg[k] = torch.div(w_avg[k], len(w))
-    print("Aggreated Weights", w_avg, flush=True)
     return w_avg
 
